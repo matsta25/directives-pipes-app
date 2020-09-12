a4434bce77ebf9a5b8242d4ecd5920fd0df828b3
matsta25 <matim2112@gmail.com>
2020-09-12 13:13:56 +0200

    feat(app): aio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes-test-app';
+  //pipes
+  public stringVariable: string = 'lorem ipsum'
+  public birthday = Date.now()
+  public jsonObject = {
+    key: 'value'
+  }
+
+  public kebabCaseVariable = 'lorem-ipsum-dolor-sit-amet'
+
+  // directives
+  public persons = ['Mathew', 'Alice', 'Bob']
+  public isAuthenticated = true;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SnakeCasePipe } from './pipe/snake-case.pipe';
+import { ChangeTextDirective } from './directive/change-text.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SnakeCasePipe,
+    ChangeTextDirective
   ],
   imports: [
     BrowserModule
